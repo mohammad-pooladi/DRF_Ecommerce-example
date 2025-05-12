6c0618c1e7ef406927ec4804101049aea8874381
Mohammad Pooladi <153905325+mohammad-pooladi@users.noreply.github.com>
2025-05-12 10:03:13 +0330

    simple change

--- main/products/models.py
+++ main/products/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 from main.common.models import BaseModel
 
-class Categore(models.Model):
+
+class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
 
+
 class Products(BaseModel):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True, blank=True, null=True)
     description = models.TextField(blank=True)
-    categore = models.ForeignKey(Categore, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
 
     class Meta:
-        ordering = ['-created_ad']
-
+        ordering = ["-created_at"]
 
     def __str__(self):
         return self.name
